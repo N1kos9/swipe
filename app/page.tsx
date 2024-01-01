af1dc2c58b7af9b79913a03ffc5922364daae0b0
N1kos9 <onkl3p@gmail.com>
2024-01-01 12:08:27 +0000

    nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>hello new project</h1>
+    <main className="w-full max-w-[1380px] px-5 md:px-10 mx-auto">
+      <nav className="mt-10 flex align-middle items-center justify-between">
+        <div>
+          <Image src="/logo.png" width={100} height={100} alt="logo" />
+        </div>
+        <div className="">
+          <Link href="/">About</Link>
+          <Link href="/">Features</Link>
+          <Link href="/">Pricing</Link>
+          <Link href="/">FAQ</Link>
+          <Link href="/">Team</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div>
+          <button>Buy now</button>
+        </div>
+      </nav>
     </main>
   );
 }
