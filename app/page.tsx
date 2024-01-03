d057af76cb37c08ba3b4e78e3f2240e866c44b9d
N1kos9 <onkl3p@gmail.com>
2024-01-03 09:30:40 +0000

    media query added

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className="w-full max-w-[1380px] mt-2 px-5 md:px-10 mx-auto">
+    <main className="w-full max-w-[1380px] mt-2 px-5 md:px-10 mx-auto ">
       {/* <Navbar /> */}
       <nav className="flex align-middle z-20 text-text font-semibold items-center justify-between">
         <div>
           <Image src="/logo.png" width={100} height={100} alt="logo" />
         </div>
-        <div>
+        <div className="">
           <Link href="/" className="">
             About
           </Link>
@@ -33,10 +33,10 @@ export default function Home() {
         </div>
         <div>
           <button className="p-4 bg-buttonBackground opacity-80 px-9 rounded-md border-2 border-background">
-            Buy noww
+            Buy now
           </button>
         </div>
-        <div className="absolute -z-20 justify-center h-full align-middle bottom-0 right-60">
+        <div className="absolute -z-20 h-full bottom-0 lg:right-40 right-[-300px] max-w-2xl max-h-2xl lg:max-w-full 	">
           <Image src="/item.png" alt="item" height={800} width={800} />
         </div>
       </nav>
